fcefd04f664a72f42bcfef75628b89d2e95bc5e5
Withalt <vn.tuankiet2004@gmail.com>
2026-07-07 10:17:20 +0700

    Add console rendering to visualize the board (Milestone 1 complete)

---
+++ Main.java
@@ -0,0 +1,10 @@
+package hazzard_chess;
+
+import hazzard_chess.model.Board;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board();
+        board.printBoard();
+    }
+}
