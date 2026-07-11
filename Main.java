baab7a0d32a649c923271d2aa0a6aa32a1016e02
Withalt <vn.tuankiet2004@gmail.com>
2026-07-12 00:41:14 +0700

    Update Main.java with console game loop for testing moves

--- Main.java
+++ Main.java
@@ -1,10 +1,23 @@
 package hazzard_chess;
 
+import java.util.Scanner;
+
 import hazzard_chess.model.Board;
+import hazzard_chess.model.Game;
 
 public class Main {
     public static void main(String[] args) {
         Board board = new Board();
+        Game game = new Game(board);
         board.printBoard();
+
+        Scanner scanner = new Scanner(System.in);
+        while(true){
+            System.out.println("Turn: " + game.getCurrentTurn());
+            System.out.print("Enter move (fromRow fromCol toRow toCol): ");
+            int fr = scanner.nextInt(), fc = scanner.nextInt(), tr = scanner.nextInt(), tc = scanner.nextInt();
+            game.movePiece(fr, fc, tr, tc);
+            board.printBoard();
+        }
     }
 }
