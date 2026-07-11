7b8fd4ff526f4e62aefa06ee6069a5f61231f871
Withalt <vn.tuankiet2004@gmail.com>
2026-07-12 00:40:53 +0700

    Add Game class: turn management and move validation

---
+++ model/Game.java
@@ -0,0 +1,62 @@
+package hazzard_chess.model;
+import java.util.List;
+
+import hazzard_chess.model.piece.Piece;
+
+public class Game {
+    private Board board;
+    private String currentTurn;
+
+    public Game(Board board){
+        this.board = board;
+        this.currentTurn = "white";
+    }
+
+    public boolean movePiece(int fromRow, int fromCol, int toRow, int toCol){
+        Square fromSquare = board.getSquare(fromRow, fromCol);
+        Piece piece = fromSquare.getPiece();
+
+        if(piece == null){
+            System.out.println("Starting square does not contain a piece.");
+            return false;
+        }
+
+        if(!piece.getColor().equals(currentTurn)){
+            System.out.println("It is not this piece's turn.");
+            return false;
+        }
+
+        List<int[]> validMoves = piece.getValidMoves(board);
+        
+        //Debug Line
+        // System.out.println("Current piece: " + piece.getSymbol() + "at (" + fromRow + "," + fromCol + ")");
+        // System.out.print("Valid moves: ");
+        // for (int[] m : validMoves){
+        //     System.out.print("(" + m[0] + "," + m[1] + ") ");
+        // }
+        // System.out.println();
+        
+        boolean isValidMove = false;
+        for (int[] move : validMoves){
+            if (move[0] == toRow && move[1] == toCol){
+                isValidMove = true;
+                break;
+            }
+        }
+        if(!isValidMove){
+            System.out.println("Invalid move.");
+            return false;
+        }
+
+        Square toSquare = board.getSquare(toRow, toCol);
+        toSquare.setPiece(piece);
+        fromSquare.setPiece(null);
+
+        currentTurn = currentTurn.equals("white") ? "black" : "white";
+        return true;
+    }
+
+    public String getCurrentTurn(){
+        return currentTurn;
+    }
+}
