8ab24e4532898b840237e4f4ce00043160e7283a
Withalt <vn.tuankiet2004@gmail.com>
2026-07-12 00:28:51 +0700

    Add Knight piece with movement logic (L-shape jumps)

--- model/piece/Knight.java
+++ model/piece/Knight.java
@@ -1,4 +1,9 @@
 package hazzard_chess.model.piece;
+import java.util.ArrayList;
+import java.util.List;
+
+import hazzard_chess.model.Board;
+import hazzard_chess.model.Square;
 
 public class Knight extends Piece {
     public Knight(String color, int row, int col){
@@ -8,5 +13,24 @@ public class Knight extends Piece {
     @Override
     public char getSymbol(){
         return getColor().equals("white") ? 'N' : 'n';
+    }
+
+    @Override
+    public List<int[]> getValidMoves(Board board){
+        List<int[]> moves = new ArrayList<>();
+        int[][] jumps = {{-2,-1}, {-2,1}, {-1,-2}, {-1,2}, {1,-2}, {1,2}, {2,-1}, {2,1}};
+        
+        for(int[] jump : jumps){
+            int r = getRow() + jump[0];
+            int c = getCol() + jump[1];
+
+            if (r>=0 && r<Board.SIZE && c>=0 && c<Board.SIZE){
+                Square target = board.getSquare(r, c);
+                if (target.isEmpty() || !target.getPiece().getColor().equals(getColor())) {
+                    moves.add(new int[]{r, c});
+                }
+            }
+        }
+        return moves;
     }
 }
